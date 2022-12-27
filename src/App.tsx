ec6455c3fe838925004162fd0ebe068266fbf132
beqa <beqamaisuradze912@gmail.com>
2022-12-27 13:40:33 +0400

    add react router and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route  } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home />}>
+    </Route>
+  )
+)
 
 function App() {
-  return <>hello</>;
+  return <><RouterProvider router={router}></RouterProvider></>;
 }
 
 export default App;
